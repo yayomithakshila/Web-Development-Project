5e346cddaab2e48ee4eb7ea9f958ed6016814baf
nuwani fernando <nuwanifernando86@outlook.com>
2025-04-07 11:33:47 +0300

    added features modifications

--- Feature2.py
+++ Feature2.py
@@ -51,6 +51,7 @@ tk.Label(root, text="Time (HH:MM):").grid(row=3, column=0)
 tk.Label(root, text="Number of users:").grid(row=4, column=0)
 
 # Other information 
+##changes done accordingly
 tk.Label(root, text="Info:").grid(row=0, column=0)
 tk.Label(root, text="Other:").grid(row=1, column=0)
 tk.Label(root, text="Date (YYYY-MM-DD):").grid(row=2, column=0)
