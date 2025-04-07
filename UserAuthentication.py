0eeecddd8779bbd7f4c2a55b01b4baf7267b7ac8
nuwanif <126983404+nuwanif@users.noreply.github.com>
2025-04-07 14:31:49 +0300

    done changes

---
+++ UserAuthentication.py
@@ -0,0 +1,5 @@
+tk.Label(root, text="Name:").grid(row=0, column=0)
+tk.Label(root, text="Email:").grid(row=1, column=0)
+tk.Label(root, text="Date (YYYY-MM-DD):").grid(row=2, column=0)
+tk.Label(root, text="Time (HH:MM):").grid(row=3, column=0)
+tk.Label(root, text="Number of Guests:").grid(row=4, column=0)
